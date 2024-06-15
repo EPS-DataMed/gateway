446fa0303798152f2efea636636ca799d16a3d45
Lucas Gabriel <lucasgabriel_df@hotmail.com>
2024-06-15 15:27:40 -0300

    refac: code

--- config.py
+++ config.py
@@ -1,10 +1,22 @@
 import os
+from dotenv import load_dotenv
 
-def get_service_url(service_name, dev_port):
-    return f"https://{service_name}.onrender.com"
+load_dotenv()
+
+def get_service_url(service_name, domain):
+    return f"https://{service_name}.{domain}"
+
+
+auth_service_name = os.getenv("AUTH_SERVICE_NAME")
+user_service_name = os.getenv("USER_SERVICE_NAME")
+file_service_name = os.getenv("FILE_SERVICE_NAME")
+data_service_name = os.getenv("DATA_SERVICE_NAME")
+
+service_domain = os.getenv("SERVICE_DOMAIN")
 
 SERVICE_URLS = {
-    "auth": get_service_url("authentication-y2qu", 8001),
-    "user": get_service_url("user-service-3uq2", 8002),
-    "data": get_service_url("data-processing-service", 8003),
+    "auth": get_service_url(auth_service_name, service_domain),
+    "user": get_service_url(user_service_name, service_domain),
+    "file": get_service_url(file_service_name, service_domain),
+    "data": get_service_url(data_service_name, service_domain)
 }
